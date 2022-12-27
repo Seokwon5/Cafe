3e270cee936c86f00d839147a76552b23d15bc6d
Seokwon5 <tjrdnjs5@gmail.com>
2022-12-27 15:36:35 +0900

    Initial Commit

---
+++ Cafe/CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeApp.swift
+//  Cafe
+//
+//  Created by 이석원 on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
